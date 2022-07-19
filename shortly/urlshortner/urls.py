68c3415ba7cf2542356601b41216164116d4f74d
alpyner7 <alpyner7@gmail.com>
2022-07-20 00:08:38 +0300

    added urlhandler app

--- shortly/urlshortner/urls.py
+++ shortly/urlshortner/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from authentication.views import home, login, signup, logout
+from urlhandler.views import dashboard, generate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('login', login, name = 'login'),
     path('signup', signup, name = 'signup'),
     path('logout', logout, name = 'logout'),
+    path('dashboard/', dashboard, name = 'dashboard'),
+    path('generate', generate, name = 'generate'),
 ]
